bb818ae680659cd54e289423ea267b15a68aee39
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-29 23:05:58 +0800

    Update Script.Cty.R

--- Script.Cty.R
+++ Script.Cty.R
@@ -4,5 +4,8 @@ df.cty <- read.csv(url(url.cty),stringsAsFactors=FALSE)
 #str(df.cty)
 df.cty2 <- df.cty[,c('official_name_en','CLDR.display.name','ISO3166.1.Alpha.2','ISO3166.1.Alpha.3','ISO3166.1.numeric','Continent','Dial','Region.Code','Region.Name')]
 colnames(df.cty2) <- c('cty','city','cty.cd2','cty.cd3','cty.cdInt','continent','dial','region.cd','region')
-df.cty2$cty.cdInt <- str_pad(df.cty2$cty.cdInt,3,pad="0")
-View(df.cty2)
+df.cty2 <- df.cty2 %>%
+            mutate(cty.cdInt = str_pad(df.cty2$cty.cdInt,3,pad="0"),
+                   cty=ifelse(cty=='United Kingdom of Great Britain and Northern Ireland'),'England',cty)
+                  )
+
