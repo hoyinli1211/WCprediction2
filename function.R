28be8458c5ed699dc9062b920aff426b441d60d2
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-30 09:23:20 +0800

    Update function.R

--- function.R
+++ function.R
@@ -12,19 +12,19 @@ regionString <- function(v.1.region,v.2.region,v.1.score,v.2.score,ind) {
   
   v.region=''
   v.score=0
-  rank1 <- regionRank(v.1,v.2)
+  rank1 <- regionRank(v.1.region,v.2.region)
   
-  if (v.1==v.2 | rank1==1) {
-    v.region <- paste(v.1,v.2,'-')
+  if (v.1.region==v.2.region | rank1==1) {
+    v.region <- paste(v.1.region,v.2.region,'-')
     v.score <- (v.1.score-v.2.score)
   }
   else {
-   v.region <- paste(v.2,v.1,'-') 
+   v.region <- paste(v.2.region,v.1.region,'-') 
    v.score <- (v.2.score-v.1.score)
   }
   
   if (ind==1) {
-    return(v.output)
+    return(v.region)
   }
   else {
     return(v.score)
