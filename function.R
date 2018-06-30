e55b573367d878047899fd29943622df605c97c9
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-30 09:43:06 +0800

    Update function.R

--- function.R
+++ function.R
@@ -1,12 +1,12 @@
 #function
 
-#region string or region.score string
 
 regionString <- function(v.1.region,v.2.region,v.1.score,v.2.score,ind) {
   
   v.region=''
   v.score=0
   rank1 <- rank(c(v.1.region,v.2.region))[1]
+  v.output <- null()
   
   if (v.1.region==v.2.region | rank1==1) {
     v.region <- paste(c(v.1.region,v.2.region),collapse='-')
@@ -17,10 +17,15 @@ regionString <- function(v.1.region,v.2.region,v.1.score,v.2.score,ind) {
     v.score <- (v.2.score-v.1.score)
   }
   
-  if (ind==1) {
-    return(v.region)
+  if (ind==0) {
+    v.output <- v.region
+  }
+  else if (ind==1) {
+    v.output <- v.score
   }
   else {
-    return(v.score)
+    v.output <- ''
   }
+  
+  return(v.output)
 }
