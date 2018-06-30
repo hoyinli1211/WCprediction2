2b72534224deb4ec0c6b80c926faefce893b18b4
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-30 08:31:22 +0800

    Update Script.Cty.R

--- Script.Cty.R
+++ Script.Cty.R
@@ -5,12 +5,12 @@ df.cty <- read.csv(url.cty,stringsAsFactors=FALSE)
 df.cty2 <- df.cty[,c('official_name_en','CLDR.display.name','ISO3166.1.Alpha.2','ISO3166.1.Alpha.3','ISO3166.1.numeric','Continent','Dial','Region.Code','Region.Name')]
 colnames(df.cty2) <- c('cty','city','cty.cd2','cty.cd3','cty.cdInt','continent','dial','region.cd','region')
 df.cty2 <- df.cty2 %>%
-            mutate(cty.cdInt = str_pad(df.cty2$cty.cdInt,3,pad="0"),
-                   cty=ifelse(cty=='United Kingdom of Great Britain and Northern Ireland','England',
-                              ifelse(cty=='Russian Federation','Russia',
-                                     ifelse(cty=='Iran (Islamic Republic of)','Iran',
-                                            ifelse(cty=='Czechia','Czech Republic',
-                                                   ifelse(cty=="Côte d'Ivoire",'Ivory Coast',cty))))),
-                   region=as.character(region)
-                   )
-
+  mutate(cty.cdInt = str_pad(df.cty2$cty.cdInt,3,pad="0"),
+         cty=ifelse(cty=='United Kingdom of Great Britain and Northern Ireland','England',
+                    ifelse(cty=='Russian Federation','Russia',
+                           ifelse(cty=='Iran (Islamic Republic of)','Iran',
+                                  ifelse(cty=='Czechia','Czech Republic',
+                                         ifelse(cty=="Côte d'Ivoire",'Ivory Coast',
+                                                ifelse(cty=='Bosnia and Herzegovina','Bosnia-Herzegovina',cty)))))),
+         region=as.character(region)
+  )
