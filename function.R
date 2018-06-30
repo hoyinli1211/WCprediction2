ab24e0c27019821069a00f22b16a21d2ad73c19a
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-30 09:16:03 +0800

    Update function.R

--- function.R
+++ function.R
@@ -7,3 +7,18 @@ regionRank <- function(v.1,v.2) {
   return(rank(c(v.1,v.2))[1])
   
 }
+
+regionRegion <- function(v.1,v.2) {
+  
+  v.output=''
+  rank1 <- regionRank(v.1,v.2)
+  
+  if (v.1==v.2 | rank1==1) {
+    v.output <- paste(v.1,v.2,'-')
+  }
+  else {
+   v.output <- paste(v.2,v.1,'-') 
+  }
+  
+  return(v.output)
+}
